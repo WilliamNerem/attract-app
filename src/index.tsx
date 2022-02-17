f5233572e0a5c9eec3c3bbb528815c0f72a4eb02
WilliamNerem <william-kn@hotmail.com>
2022-02-17 12:14:04 +0100

    AA1: Added first component and general cleanup

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import { Button } from "./components/atoms/button";
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <Button buttonTest={'Button'}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
