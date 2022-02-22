b8819a7f81725ddc6d7b1777a69450f925573f5a
mbrandal <brandalmartin@gmail.com>
2022-02-22 15:52:23 +0100

    AA-5: Completed progressbar component

---
+++ src/components/atoms/progressbar.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import '../../styles/progressbar.css'
+
+interface ProgressBarProps {
+    completed: number
+}
+
+export const ProgressBar = ({
+    completed
+}: ProgressBarProps) => {
+    return (
+        <div className='progressContainer'>
+            <div className='progressFiller' style={ { width: `${ completed }%` } }>
+                <span className='progressLabel'>{`${completed}%`}</span>
+            </div>
+        </div>
+    );
+};
+
+export default ProgressBar;
--- src/pages/valgomat.tsx
+++ src/pages/valgomat.tsx
@@ -1,10 +1,12 @@
 import { Navbar } from "../components/molecule/navbar";
+import { ProgressBar } from "../components/atoms/progressbar";
 
 const valgomat = () => {
     return (
         <>
             <Navbar/>
             <h1>VALGOMAT!!</h1>
+            <ProgressBar completed={50}/>
         </>
     );
 };
