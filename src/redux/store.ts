724fa3c2cfb0161634f3a83155491360b97e449d
WilliamNerem <william-kn@hotmail.com>
2022-02-28 16:26:57 +0100

    AA-23: Added redux to inspect element in browser

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,9 +1,9 @@
 import { createStore, applyMiddleware } from "redux";
 import { reducers } from "./reducers";
 import thunk from 'redux-thunk'
+import { composeWithDevTools } from "redux-devtools-extension";
 
 export const store = createStore(
     reducers,
-    {},
-    applyMiddleware(thunk)
+    composeWithDevTools(applyMiddleware(thunk))
 );
