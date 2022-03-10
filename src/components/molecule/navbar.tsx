acbe399037b6278114bf63c04271fbc4016ee70e
Maren <maren.lund@wemail.no>
2022-03-10 13:31:15 +0100

    AA-42: WIP

--- src/components/molecule/navbar.tsx
+++ src/components/molecule/navbar.tsx
@@ -3,18 +3,26 @@ import { HamburgerMenu } from "./hamburger";
 import Acc_Logo_Black_Purple_RGB from '../../images/Acc_Logo_Black_Purple_RGB.png'
 import '../../styles/navbar.style.css'
 import Backdrop from '@mui/material/Backdrop';
+import {AlertDialog} from "../atoms/alertDialogFunction";
+import {useSelector} from "react-redux";
+import {State} from "../../redux/reducers";
 
 export const Navbar = () => {
     const [open, setOpen] = useState(false);
+    const counter = useSelector((state: State) => state.questionCounter);
     const handleToggle = () => {
         setOpen(!open);
     };
+
+    const handleClick = () => {
+
+    }
 
 
     return(
         <div className='navMargin'>
             <div className='nav'>
-                <a href={'/'}><img className='logo' src={Acc_Logo_Black_Purple_RGB} alt='logo'/></a>
+                <a onClick={handleClick}><img className='logo' src={Acc_Logo_Black_Purple_RGB} alt='logo'/></a>
                 <HamburgerMenu onClick={handleToggle}/>
             </div>
             <Backdrop
