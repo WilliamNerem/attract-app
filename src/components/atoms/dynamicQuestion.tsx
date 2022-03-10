740a03177ee40c13dfa06af2b60f43414f762d8a
mbrandal <brandalmartin@gmail.com>
2022-03-10 12:30:23 +0100

    AA-35: Added quick demo dummy info for dynamic questions

--- src/components/atoms/dynamicQuestion.tsx
+++ src/components/atoms/dynamicQuestion.tsx
@@ -11,19 +11,25 @@ export const DynamicQuestion = ({firstDep, secondDep}: DynamicQuestionProps) => 
     if (firstDep + secondDep === 3) { //Then we know it's tech vs interactive
         return (
             <div className={'dynamicQuestion'}>
-                <p>TECH OR INTERACTIVE?</p>
+                <h4>Trykk på den påstanden som passer best</h4>
+                <p>Det viktigste for meg er at produktet fungerer godt ved hjelp av en smart teknisk løsning</p>
+                <p>Det er viktig for meg at et produkt ser bra ut</p>
             </div>
         )
     } else if (firstDep + secondDep === 2) {
         return (
             <div className={'dynamicQuestion'}>
-                <p>STRAT OR INTERACTIVE?</p>
+                <h4>Trykk på den påstanden som passer best</h4>
+                <p>Jeg liker business</p>
+                <p>Jeg liker design</p>
             </div>
         )
     } else if (firstDep + secondDep === 1) {
         return (
             <div className={'dynamicQuestion'}>
-                <p>STRAT OR TECH?</p>
+                <h4>Trykk på den påstanden som passer best</h4>
+                <p>Jeg jobber gjerne på et prosjekt i lang tid</p>
+                <p>Jeg foretrekker å bytte arbeidsmiljø ofte</p>
             </div>
         )
     }
