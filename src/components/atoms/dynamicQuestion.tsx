33acfa81d93f5a19a9fbd6e18fd97015387bc648
mbrandal <brandalmartin@gmail.com>
2022-03-14 12:03:43 +0100

    AA-31-2: Close to implementing dynamic questions

--- src/components/atoms/dynamicQuestion.tsx
+++ src/components/atoms/dynamicQuestion.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
 import '../../styles/valgomat.style.css'
+import {useDispatch, useSelector} from "react-redux";
+import {actionCreators, State} from "../../redux";
+import {bindActionCreators} from "redux";
+import {Result} from "../organisms/result";
+
 
 
 interface DynamicQuestionProps {
@@ -8,28 +13,55 @@ interface DynamicQuestionProps {
 }
 
 export const DynamicQuestion = ({firstDep, secondDep}: DynamicQuestionProps) => {
-    if (firstDep + secondDep === 3) { //Then we know it's tech vs interactive
+    const dispatch = useDispatch();
+    const departmentsArray = useSelector((state: State) => state.departmentsAlgorithm);
+    const { decreaseCounter } = bindActionCreators(actionCreators, dispatch);
+    let done = false;
+    let pointsArray: number[] = [];
+
+    const checkClicked = (chosenDepartment: number) => {
+        departmentsArray.map((differenceCharacteristic, index) => {
+            pointsArray[index] = departmentsArray[index].points;
+        });
+
+        pointsArray[chosenDepartment] += 1;
+        decreaseCounter();
+        done = true;
+
+    }
+    if(done) {
+        return (
+            <>
+                <Result totalPointsArray={pointsArray}/>
+            </>
+        )
+    }
+
+    else if (firstDep + secondDep === 3) { //Then we know it's tech vs interactive
         return (
             <div className={'dynamicQuestion'}>
                 <h4>Trykk på den påstanden som passer best</h4>
-                <p>Det viktigste for meg er at produktet fungerer godt ved hjelp av en smart teknisk løsning</p>
-                <p>Det er viktig for meg at et produkt ser bra ut</p>
+                <button className='valgomatButton' onClick={() => checkClicked(1)}>Det viktigste for meg er at produktet fungerer godt ved hjelp av en smart teknisk løsning</button>
+                <button className='valgomatButton' onClick={() => checkClicked(2)}>Det er viktig for meg at et produkt ser bra ut</button>
+                <button className='valgomatButton' onClick={() => decreaseCounter()}>Forrige spørsmål</button>
             </div>
         )
     } else if (firstDep + secondDep === 2) {
         return (
             <div className={'dynamicQuestion'}>
                 <h4>Trykk på den påstanden som passer best</h4>
-                <p>Jeg liker business</p>
-                <p>Jeg liker design</p>
+                <button className='valgomatButton' onClick={() => checkClicked(0)}>Jeg liker business</button>
+                <button className='valgomatButton' onClick={() => checkClicked(2)}>Jeg liker design</button>
+                <button className='valgomatButton' onClick={() => decreaseCounter()}>Forrige spørsmål</button>
             </div>
         )
     } else if (firstDep + secondDep === 1) {
         return (
             <div className={'dynamicQuestion'}>
                 <h4>Trykk på den påstanden som passer best</h4>
-                <p>Jeg jobber gjerne på et prosjekt i lang tid</p>
-                <p>Jeg foretrekker å bytte arbeidsmiljø ofte</p>
+                <button className='valgomatButton' onClick={() => checkClicked(0)}>Jeg foretrekker å bytte arbeidsmiljø ofte</button>
+                <button className='valgomatButton' onClick={() => checkClicked(1)}>Jeg jobber gjerne på et prosjekt i lang tid</button>
+                <button className='valgomatButton' onClick={() => decreaseCounter()}>Forrige spørsmål</button>
             </div>
         )
     }
--- src/pages/valgomat.tsx
+++ src/pages/valgomat.tsx
@@ -1,7 +1,6 @@
 import { Navbar } from "../components/molecule/navbar";
 import { ProgressBar } from "../components/atoms/progressbar";
 import { Questions } from "../components/atoms/questions";
-import { ValgomatButton } from "../components/atoms/valgomatButton";
 import { LikertScale } from "../components/atoms/likertScale";
 import { AlertDialog } from "../components/atoms/alertDialogFunction";
 import * as React from "react";
@@ -41,7 +40,6 @@ const Valgomat = () => {
 
     for (let questions of QuestionsData()) {
         if (counter === questions.questionNumber) {
-
             return (
                 <>
                     <div className='valgomat'>
@@ -55,7 +53,6 @@ const Valgomat = () => {
             )
         }
         if (counter === QuestionsData().length + 1) {
-
             let totalPoints: number[] = [];
 
             userDifferences.map((differenceCharacteristic, index) => {
@@ -79,7 +76,6 @@ const Valgomat = () => {
                         <Navbar/>
                         <h1 className='questionNumber'>Spørsmål {counter}</h1>
                         <DynamicQuestion firstDep={firstDep} secondDep={secondDep}/>
-                        <ValgomatButton/>
                         <ProgressBar completed={100}/>
                     </>
                 )
