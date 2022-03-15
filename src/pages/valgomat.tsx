1255d37f302da567e380f15a41c6e755fac7d5c7
mbrandal <brandalmartin@gmail.com>
2022-03-15 12:38:54 +0100

    AA-31-2: Removed console.log

--- src/pages/valgomat.tsx
+++ src/pages/valgomat.tsx
@@ -36,7 +36,6 @@ const Valgomat = () => {
             <AlertDialog/>
         )
     }
-    console.log("Rendres det?");
 
     for (let questions of QuestionsData()) {
         if (counter === questions.questionNumber) {
