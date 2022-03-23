8a3ce9156f469286d059bcad264e0c73d997f01b
mbrandal <brandalmartin@gmail.com>
2022-03-23 15:33:46 +0100

    AA-67: Made noPage for application

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+// import {BrowserRouter, HashRouter, Route, Routes} from "react-router-dom";
 import Home from "./pages/home";
 import Valgomat from "./pages/valgomat";
 import Info from "./pages/info";
@@ -12,18 +12,14 @@ import InvalidNotFoundPage from "./components/atoms/invalidNotFoundPage";
 export default function App() {
     return (
         <Provider store={store}>
-            <BrowserRouter>
+            <HashRouter>
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="valgomat" element={<Valgomat />} />
                     <Route path="info" element={<Info />} />
                     <Route path="*" element={<InvalidNotFoundPage />} />
-                    <Route path="165.227.130.116/" element={<Home />} />
-                    <Route path="165.227.130.116/valgomat" element={<Valgomat />} />
-                    <Route path="165.227.130.116/info" element={<Info />} />
-                    <Route path="165.227.130.116/*" element={<InvalidNotFoundPage />} />
                 </Routes>
-            </BrowserRouter>
+            </HashRouter>
         </Provider>
     );
 }
