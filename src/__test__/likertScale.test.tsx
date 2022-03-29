976ce14ae5f376be62e624c829d31e8621998137
WilliamNerem <william-kn@hotmail.com>
2022-03-29 11:28:46 +0200

    AA-59: Added test for likertScale

---
+++ src/__test__/likertScale.test.tsx
@@ -0,0 +1,78 @@
+import React from 'react';
+import ReactDOM from "react-dom";
+import { render, cleanup, fireEvent } from "@testing-library/react";
+import {LikertScale} from "../components/atoms/likertScale";
+import {Provider} from "react-redux";
+import {store} from "../redux";
+
+// mocking of component render - not necessary unless error with "found multiple element with data-testid"
+afterEach(cleanup);
+
+describe('Liker scale render', () => {
+
+    it('should render without crashing', () => {
+        const div = document.createElement("div");
+        ReactDOM.render(
+            <Provider store={store}>
+                <LikertScale questionNumber={1} characteristic={() => {}} isReversed={false} />
+            </Provider>
+            , div
+
+        );
+    });
+
+    it('should render likert scale with correct values', () => {
+        const { getByTestId } = render(
+            <Provider store={store}>
+                <LikertScale questionNumber={1} characteristic={() => {}} isReversed={false} />
+            </Provider>
+        );
+        expect(getByTestId("likertScale")).toHaveTextContent('Helt uenig');
+        expect(getByTestId("likertScale")).toHaveTextContent('Nøytral');
+        expect(getByTestId("likertScale")).toHaveTextContent('Helt enig');
+    });
+
+    it('should update state on click', () => {
+        let points = 5;
+        const func = (num: number, bool: boolean) => {
+            if (bool){
+                points = points - num;
+            } else {
+                points = points + num;
+            }
+        };
+
+        const { getByTestId } = render(
+            <Provider store={store}>
+                <LikertScale questionNumber={1} characteristic={func} isReversed={false} />
+            </Provider>
+        );
+
+        fireEvent.click(getByTestId('likertScaleStronglyDisagree'));
+        expect(points).toBe(3);
+        fireEvent.click(getByTestId('likertScaleModeratelyAgree'));
+        expect(points).toBe(6);
+    });
+
+    it('should update state on click when isReversed is true', () => {
+        let points = 5;
+        const func = (num: number, bool: boolean) => {
+            if (bool){
+                points = points - num;
+            } else {
+                points = points + num;
+            }
+        };
+
+        const { getByTestId } = render(
+            <Provider store={store}>
+                <LikertScale questionNumber={1} characteristic={func} isReversed={true} />
+            </Provider>
+        );
+
+        fireEvent.click(getByTestId('likertScaleStronglyDisagree'));
+        expect(points).toBe(7);
+        fireEvent.click(getByTestId('likertScaleModeratelyAgree'));
+        expect(points).toBe(4);
+    });
+});
