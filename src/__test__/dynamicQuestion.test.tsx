ce5f5850698e067866fe518706f48e166ceadb66
WilliamNerem <william-kn@hotmail.com>
2022-03-29 10:39:38 +0200

    AA-59: Small update to dynamicQuestion test

--- src/__test__/dynamicQuestion.test.tsx
+++ src/__test__/dynamicQuestion.test.tsx
@@ -21,42 +21,36 @@ describe('Dynamic Question render', () => {
     });
 
     it('should render tech vs interactive when 1 + 2', () => {
-        const { container } = render(
+        const { getByTestId } = render(
             <Provider store={store}>
                 <DynamicQuestion firstDep={1} secondDep={2} />
             </Provider>
         );
-        const buttons = container.querySelectorAll('button')
 
-        expect(buttons[0].textContent).toBe("Det viktigste for meg er at produktet fungerer godt ved hjelp av en smart teknisk løsning");
-        expect(buttons[1].textContent).toBe("Det er viktig for meg at et produkt ser bra ut");
-        expect(buttons[2].textContent).toBe("Forrige spørsmål");
+        expect(getByTestId("dynamicQuestionOne")).toHaveTextContent("Det viktigste for meg er at produktet fungerer godt ved hjelp av en smart teknisk løsning");
+        expect(getByTestId("dynamicQuestionTwo")).toHaveTextContent("Det er viktig for meg at et produkt ser bra ut");
     });
 
     it('should render strat vs interactive when 0 + 2', () => {
-        const { container } = render(
+        const { getByTestId } = render(
             <Provider store={store}>
                 <DynamicQuestion firstDep={0} secondDep={2} />
             </Provider>
         );
-        const buttons = container.querySelectorAll('button')
 
-        expect(buttons[0].textContent).toBe("Jeg liker business");
-        expect(buttons[1].textContent).toBe("Jeg liker design");
-        expect(buttons[2].textContent).toBe("Forrige spørsmål");
+        expect(getByTestId("dynamicQuestionOne")).toHaveTextContent("Jeg liker business");
+        expect(getByTestId("dynamicQuestionTwo")).toHaveTextContent("Jeg liker design");
     });
 
     it('should render strat vs tech when 0 + 1', () => {
-        const { container } = render(
+        const { getByTestId } = render(
             <Provider store={store}>
                 <DynamicQuestion firstDep={0} secondDep={1} />
             </Provider>
         );
-        const buttons = container.querySelectorAll('button')
 
-        expect(buttons[0].textContent).toBe("Jeg foretrekker å bytte arbeidsmiljø ofte");
-        expect(buttons[1].textContent).toBe("Jeg jobber gjerne på et prosjekt i lang tid");
-        expect(buttons[2].textContent).toBe("Forrige spørsmål");
+        expect(getByTestId("dynamicQuestionOne")).toHaveTextContent("Jeg foretrekker å bytte arbeidsmiljø ofte");
+        expect(getByTestId("dynamicQuestionTwo")).toHaveTextContent("Jeg jobber gjerne på et prosjekt i lang tid");
     });
 
 });
