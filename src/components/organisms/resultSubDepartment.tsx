22e8542f2b0d120444ecbadb69783d6142017d3e
Maren <maren.lund@wemail.no>
2022-04-10 12:50:41 +0200

    AA-71: added array for sub departments and displayed on sub result page

--- src/components/organisms/resultSubDepartment.tsx
+++ src/components/organisms/resultSubDepartment.tsx
@@ -7,6 +7,7 @@ import {Navbar} from "../molecule/navbar";
 import {useDispatch} from "react-redux";
 import {bindActionCreators} from "redux";
 import {actionCreators} from "../../redux";
+import {SubDepartments} from "../../subDepartments";
 
 interface resultSubDepProps {
     information: any[]
@@ -19,11 +20,27 @@ export const ResultSubDepartment = ({information
     const { valgomatIsInProgress } = bindActionCreators(actionCreators, dispatch);
     const [carousel, setCarousel] = useState({first: 'leftCard', second: 'middleCard', third: 'rightCard'});
     const [disabledButtons, setDisabledButtons] = useState('');
+    const [topThreeDep, setTopThreeDep] = useState([0, 0, 0]);
     valgomatIsInProgress(false);
-    const sortedArray = information.sort((a, b) => (a.points > b.points) ? 1 : -1); //sorts array that is sent in as prop, by points, finding the three with most points
-    console.log(sortedArray)
+    const sortedArray = information.sort((a, b) => (a.points < b.points) ? 1 : -1); //sorts array that is sent in as prop, by points, finding the three with most points
+    const subDepArray = SubDepartments();
 
-    //have to: make a array with every sub department with info so that we can get the info based on the sorted array above and use it in the return
+    useEffect(() => {
+        let arr: any[] | ((prevState: number[]) => number[]) = [];
+        for (let subDep of subDepArray) {
+            if (sortedArray[0].subdivision === subDep.id) {
+                console.log('Første plass: ' + sortedArray[0].subdivision + subDep.id + subDepArray.indexOf(subDep))
+                arr.splice(0, 0, subDepArray.indexOf(subDep))
+            }
+            if (sortedArray[1].subdivision === subDep.id) {
+                arr.splice(1, 0, subDepArray.indexOf(subDep))
+            }
+            if (sortedArray[2].subdivision === subDep.id) {
+                arr.splice(2, 0, subDepArray.indexOf(subDep))
+            }
+        }
+        setTopThreeDep(arr);
+    }, []);
 
     useEffect(() => {
         setDisabledButtons('disabledButtons');
@@ -47,49 +64,49 @@ export const ResultSubDepartment = ({information
     return(
         <div data-testid={'resultComponent'} className='wrapper'>
             <Navbar/>
-            {/*<div className='result'>*/}
-            {/*    <div className='gradientDiv'>*/}
-            {/*        <ResultText result={''}/>*/}
-            {/*        <p className={'sub-dep'}>{information[0].title+', '+information[1].title+', '+information[2].title}</p>*/}
-            {/*    </div>*/}
-            {/*    <div className='carousel'>*/}
-            {/*        <div className={carousel.first+' leftCarouselItem'}>*/}
-            {/*            <InfoCard*/}
-            {/*                heading={information[0].title}*/}
-            {/*                link={information[0].link}*/}
-            {/*                text={information[0].infoText}*/}
-            {/*                subHeading={'Veien videre'}*/}
-            {/*                subText={information[0].infoSubText}*/}
-            {/*                linkText={'Les mer om '+information[0].title}*/}
-            {/*            />*/}
-            {/*        </div>*/}
-            {/*        <div className={carousel.second+' middleCarouselItem'}>*/}
-            {/*            <InfoCard*/}
-            {/*                heading={information[1].title}*/}
-            {/*                link={information[1].link}*/}
-            {/*                text={information[1].infoText}*/}
-            {/*                subHeading={'Veien videre'}*/}
-            {/*                subText={information[1].infoSubText}*/}
-            {/*                linkText={'Les mer om '+information[1].title}*/}
-            {/*            />*/}
-            {/*        </div>*/}
-            {/*        <div className={carousel.third+' rightCarouselItem'}>*/}
-            {/*            <InfoCard*/}
-            {/*                heading={information[2].title}*/}
-            {/*                link={information[2].link}*/}
-            {/*                text={information[2].infoText}*/}
-            {/*                subHeading={'Veien videre'}*/}
-            {/*                subText={information[2].infoSubText}*/}
-            {/*                linkText={'Les mer om '+information[2].title}*/}
-            {/*            />*/}
-            {/*        </div>*/}
-            {/*        <a className={disabledButtons+' leftArrow'} onClick={handleLeftArrow} />*/}
-            {/*        <a className={disabledButtons+' rightArrow'} onClick={handleRightArrow}/>*/}
-            {/*    </div>*/}
-            {/*    <div className='buttonDiv'>*/}
-            {/*        <Button href='/' text='Tilbake til forsiden'/>*/}
-            {/*    </div>*/}
-            {/*</div>*/}
+            <div className='result'>
+                <div className='gradientDiv'>
+                    <ResultText result={''}/>
+                    <p className={'sub-dep'}>{subDepArray[topThreeDep[0]].title+', '+subDepArray[topThreeDep[1]].title+', '+subDepArray[topThreeDep[2]].title}</p>
+                </div>
+                <div className='carousel'>
+                    <div className={carousel.first+' leftCarouselItem'}>
+                        <InfoCard
+                            heading={subDepArray[topThreeDep[2]].title}
+                            link={subDepArray[topThreeDep[2]].link}
+                            text={subDepArray[topThreeDep[2]].infoText}
+                            subHeading={'Veien videre'}
+                            subText={subDepArray[topThreeDep[2]].infoSubText}
+                            linkText={'Les mer om '+subDepArray[topThreeDep[2]].title}
+                        />
+                    </div>
+                    <div className={carousel.second+' middleCarouselItem'}>
+                        <InfoCard
+                            heading={subDepArray[topThreeDep[0]].title}
+                            link={subDepArray[topThreeDep[0]].link}
+                            text={subDepArray[topThreeDep[0]].infoText}
+                            subHeading={'Veien videre'}
+                            subText={subDepArray[topThreeDep[0]].infoSubText}
+                            linkText={'Les mer om '+subDepArray[topThreeDep[0]].title}
+                        />
+                    </div>
+                    <div className={carousel.third+' rightCarouselItem'}>
+                        <InfoCard
+                            heading={subDepArray[topThreeDep[1]].title}
+                            link={subDepArray[topThreeDep[1]].link}
+                            text={subDepArray[topThreeDep[1]].infoText}
+                            subHeading={'Veien videre'}
+                            subText={subDepArray[topThreeDep[1]].infoSubText}
+                            linkText={'Les mer om '+subDepArray[topThreeDep[1]].title}
+                        />
+                    </div>
+                    <a className={disabledButtons+' leftArrow'} onClick={handleLeftArrow} />
+                    <a className={disabledButtons+' rightArrow'} onClick={handleRightArrow}/>
+                </div>
+                <div className='buttonDiv'>
+                    <Button href='/' text='Tilbake til forsiden'/>
+                </div>
+            </div>
         </div>
     );
 };
