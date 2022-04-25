b1f4c67a048d4a3efc3f1d06615d2ef76cb0cc70
WilliamNerem <william-kn@hotmail.com>
2022-04-25 13:45:54 +0200

    AA-16: Added flags in hamburger

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -4,30 +4,16 @@ import '../styles/home.style.css';
 import { InfoCard } from "../components/atoms/infoCard";
 import { ResetStates } from "../resetStates";
 import { Footer } from "../components/molecule/footer";
-import i18n from '../i18n';
 import { useTranslation } from "react-i18next";
-import {useState} from "react";
-import Text from "../components/atoms/text";
 
 const Home = () => {
-    const [locale, setLocale] = useState(i18n.language);
-    i18n.on('languageChanged', (lng) => setLocale(i18n.language));
     ResetStates();
-
-  const changeLocale = (l: string) =>  {
-        if (locale !== l) {
-            i18n.changeLanguage(l);
-        }
-    }
     const { t } = useTranslation();
     return (
         <>
             <Navbar/>
             <div className='home'>
-                <Text></Text>
                 <div className='imageDiv'>
-                    <button onClick={() => changeLocale('en')}>English </button>
-                    <button onClick={() => changeLocale('no')}>Norsk </button>
                     <h1 className='headerHome'>{t('headerHome')}</h1>
                     <div className='startWrapper'>
                         <Link to="/valgomat" className='start' >{t('start')}</Link>
